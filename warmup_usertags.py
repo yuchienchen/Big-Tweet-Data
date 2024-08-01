15042a70cf703291640a1c083485c683204be62d
yuchienchen <yuchienc@yahoo.com>
2024-08-01 18:47:34 -0500

    Initialize

--- warmup_usertags.py
+++ warmup_usertags.py
@@ -1,5 +1,21 @@
 def main():   
-    {'@BarackObama': {'#SCOTUS': 4, '#Obamacare': 3}}
+    user_tags = {'@BarackObama': {'#SCOTUS': 4, '#Obamacare': 3}}
+
+    user_tags['@kanyewest'] = {}
+
+    print(user_tags)
+
+    if user_tags[key][key] in user_tags:
+        value = user_tags[key][key] += 1
+    
+    # counts = user_tags[user_name]
+    # value = counts[item]
+
+    # if user_name not in user_tags:
+    #     user_tags[user_name] = {}
+    
+    # if item in counts:
+
 
 
 
